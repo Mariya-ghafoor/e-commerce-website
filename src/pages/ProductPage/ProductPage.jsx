aed7e930181d9e158402428948bc654e2bdb3bfe
Mariya Kashif <mariyakashif@Mariyas-MacBook-Air.local>
2023-10-30 20:08:42 +1100

    corrects case of import

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getProductById } from "../../services/productsService";
-import DisplayProductById from "../../components/displayProductById/displayProductById.jsx";
+import DisplayProductById from "../../components/DisplayProductById/displayProductById.jsx";
 
 import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
 
